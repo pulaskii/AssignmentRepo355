ccb25889134f8451a3741ca33a63ebce0d3be6be
pulaskii <jvinsonxxiv@gmail.com>
2025-10-29 15:09:27 -0400

    start creating sql python files

--- Sprint 1/sqlCode/createTables.sql
+++ Sprint 1/sqlCode/createTables.sql
@@ -6,6 +6,8 @@ CREATE TABLE Users (
     MiddleNameOrInitial VARCHAR(50) NOT NULL,
     LastName VARCHAR(50),
 
+    PasswordHash VARCHAR(100) NOT NULL,
+
     Age INT CHECK(Age >= 18 AND Age <= 99),
     Phone INT(10),
     PatientOrDoctor VARCHAR(7) CHECK(PatientOrDoctor == 'patient' or PatientOrDoctor == 'doctor')
