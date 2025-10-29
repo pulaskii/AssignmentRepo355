4420a09178782ac4717ad71bcdb3c8cdc886b486
Owen Cosner <ogc276@gmail.com>
2025-10-29 13:26:33 -0400

    Added email validation for sign up page Hid passwords Added confirmation password field, works fucnctionally but needs front end work

--- Sprint 1/Flask Backend/main.py
+++ Sprint 1/Flask Backend/main.py
@@ -3,8 +3,9 @@ from flask_bcrypt import Bcrypt
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField, PasswordField, SubmitField, validators
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, SubmitField, validators
+from wtforms.validators import EqualTo
 import jinja2
 
 app = Flask(__name__)
@@ -23,6 +24,7 @@ def patient_signup():
     phone_number = None
     user_email = None
     password = None
+    confirm_password = None
 
     sign_up_form = SignUp()
     if request.method == "POST":
@@ -32,9 +34,11 @@ def patient_signup():
             phone_number = sign_up_form.phone_number.data
             user_email = sign_up_form.user_email.data
             password = sign_up_form.password.data
+            confirm_password = sign_up_form.confirm_password.data
 
     return render_template("patient_signup.html", first_name = first_name, last_name = last_name,
-                           phone_number = phone_number, user_email = user_email, password = password, form = sign_up_form)
+                           phone_number = phone_number, user_email = user_email, password = password, confirm_password = confirm_password, 
+                           form = sign_up_form)
 
 @app.route("/provider")
 def provider_portal():
@@ -42,16 +46,24 @@ def provider_portal():
 
 class SignUp(FlaskForm):
     first_name = StringField("Enter your first name", validators = [validators.DataRequired(message = "First name is required")], render_kw = {'placeholder': "John"})
+    
     last_name = StringField("Enter your last name", validators = [validators.DataRequired(message = "Last name is required")], render_kw = {'placeholder': "Doe"})
+    
     phone_number = StringField("Phone number", validators = [validators.DataRequired(message = "Phone number is required"),
                                                                validators.Length(min = 10, message = "Must be at least 10 characters"),
                                                                validators.Regexp(r'^$\d{3}$ \d{3}-\d{4}$', message = 'Invalid phone number format')
                                                              ], render_kw = {'placeholder': "(000) 000-0000"})
-    user_email = StringField("Email", validators = [validators.DataRequired()], render_kw = {'placeholder': "example@email.com"})
-    password = StringField("Create a unique password.", validators = [validators.DataRequired(message = "Please enter a password"),
-                                                                                                                                      validators.Length(min = 8, max = 20, message = "Please enter a password between 8 and 20 characters"),
-                                                                                                                                      validators.Regexp(r'^(?=.*[A-Z])(?=.*[!@#$%^+=-])(?=.{8,20}$)[^{}[\]<|*&"()]*$', message = "Please enter a valid password. Valid special characters are @, #, !, $, -, or _")],
+    user_email = StringField("Email", validators = [validators.DataRequired(), validators.Email("Must be a valid email")], render_kw = {'placeholder': "example@email.com"})
+    
+    password = PasswordField("Create a unique password.", validators = [validators.DataRequired(message = "Please enter a password"),
+                validators.Length(min = 8, max = 20, message = "Please enter a password between 8 and 20 characters"),
+                validators.Regexp(r'^(?=.*[A-Z])(?=.*[!@#$%^+=-])(?=.{8,20}$)[^{}[\]<|*&"()]*$', message = "Please enter a valid password. Valid special characters are @, #, !, $, -, or _"),
+                EqualTo('confirm_password', message="Passwords must match")],
                            render_kw = {'placeholder': "8-20 characters long, one capital letter, one special character"})
+    
+    confirm_password = PasswordField("Confirm your password", validators = [validators.DataRequired(message="Please confirm your password")
+    ], render_kw={'placeholder': "Re-enter your password"})
+
     submit_button = SubmitField("Submit")
 
 if __name__ == "__main__":
