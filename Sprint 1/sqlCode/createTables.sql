a82840eb09588b01ab9784847124a21d8aa71749
pulaskii <jvinsonxxiv@gmail.com>
2025-10-31 14:12:44 -0400

    change some sql code

--- Sprint 1/sqlCode/createTables.sql
+++ Sprint 1/sqlCode/createTables.sql
@@ -8,9 +8,11 @@ CREATE TABLE Users (
 
     PasswordHash VARCHAR(100) NOT NULL,
 
-    Age INT CHECK(Age >= 18 AND Age <= 99),
-    Phone INT(10),
-    PatientOrProvider VARCHAR(8) CHECK(PatientOrProvider == 'Patient' or PatientOrProvider == 'Provider')
+    Age INT,
+    CHECK(Age >= 18 AND Age <= 99),
+    
+    Phone INT,
+    
+    PatientOrProvider VARCHAR(8),
+    CHECK(PatientOrProvider = 'Patient' or PatientOrProvider = 'Provider')
 )
-
-
