e0df382f51431a542efd2fd5d46a793bf0ab7996
pulaskii <jvinsonxxiv@gmail.com>
2025-11-17 15:59:51 -0500

    add some new fill data for tests

--- Sprint 1/sqlCode/fillDbForTests.sql
+++ Sprint 1/sqlCode/fillDbForTests.sql
@@ -1,6 +1,13 @@
-INSERT INTO users (Email, IsRegistered, FirstName, MiddleNameOrInitial, LastName,
-    Age, Phone, PatientOrDoctor)
+INSERT INTO users (Email, FirstName, LastName, PatientOrProvider, PasswordHash)
 
-VALUES  (),  
-        (),
-        ();
+VALUES  ('bob@bob.com', 'Bobf', 'Bobl', 'patient', '$2b$12$Vam60GL5xHEvpq/6EfMINOByfVe/7.tBwWBY/jhIPgroXT28yxUmO'),  
+        ('sally@sally.com', 'Sallyf', 'Sallyl', 'provider', '$2b$12$Vam60GL5xHEvpq/6EfMINOByfVe/7.tBwWBY/jhIPgroXT28yxUmO'),
+        ('jimothy@jimothy.com', 'jimothyf', 'jimmothyl', 'patient', '$2b$12$Vam60GL5xHEvpq/6EfMINOByfVe/7.tBwWBY/jhIPgroXT28yxUmO');
+
+
+
+INSERT INTO accessMap (connectionUID, doctor, patient)
+
+VALUES  ('1', 'sally@sally.com','bob@bob.com'),
+        ('2','sally@sally.com','jimothy@jimothy.com');
+
